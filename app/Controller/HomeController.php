2bf7d8011c343d778f2eddfaa2a6c1d813b4c718
Mirlani <laniwambuliga@gmail.com>
2023-05-16 18:34:15 +0700

    membuat model

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -8,6 +8,10 @@ class HomeController
 
     public function Index()
     {
+        $model = [
+            "title" => "Belajar PHP MVC",
+            "content" => "Selamat Belajar php mvc"
+        ];
         echo "HomeController.index";
     }
 
@@ -24,6 +28,21 @@ class HomeController
     public function about()
     {
         echo "Author: Muhammad Mirlani";
+    }
+
+    public function login(): void
+    {
+        $reques = [
+            "username" => $_POST["username"],
+            "password" => $_POST["password"]
+        ];
+
+        $respon = [
+            "pesan" => "login sukses"
+        ];
+
+        // kirimkan respon ke view
+        // intinya itu data apapun adalah model
     }
     
 }
